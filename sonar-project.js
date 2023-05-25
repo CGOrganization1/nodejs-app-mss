9fa6a59ebd8fe294bb3791694c8912404b56fe3c
Srinivas Pendela <110053859+Srinidevopsgit@users.noreply.github.com>
2023-05-25 15:13:26 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://35.180.61.112:9000/',
+  serverUrl: 'http://172.31.44.124:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	    'sonar.login': 'squ_ea242826a04b9a6b8818f9f594f81dd8a5979a5e',
+	    'sonar.login': 'sqa_4c0a5f02b523539f1f136973faedd8c9d886bf29',
 	    //'sonar.login': 'admin',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
